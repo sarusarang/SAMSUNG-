bee19ea278c6422dca5d9778e3fcfcd5ef28b8d8
sarusarang <sarangsaru445@gmail.com>
2024-02-13 13:54:10 +0530

    mobile view

---
+++ main.js
@@ -0,0 +1,74 @@
+
+// MOBILE VIEW 
+
+
+// img-1
+function MobVideo()
+{
+
+  var windowWidth = window.innerWidth;
+  
+  if(windowWidth < 481)
+  {
+    document.getElementById('mob').src ='Img/MOBILE VIEW.webm';
+  }
+ 
+}
+
+
+
+var windowWidth = window.innerWidth;
+  if(windowWidth < 481)
+  {
+    MobVideo()
+  }
+
+
+
+
+// // img- 2
+
+  function MobImg()
+{
+
+  var windowWidth = window.innerWidth;
+
+  if(windowWidth < 481)
+  {
+    document.getElementById('mobimg1').src ='https://images.samsung.com/is/image/samsung/assets/in/eureka2024/HOME_Live-Translate_Main-KV_720x1080_mo.jpg?imwidth=720';
+  }
+ 
+}
+
+
+
+var windowWidth = window.innerWidth;
+  if(windowWidth < 481)
+  {
+    MobImg()
+  }
+
+
+// img - 3
+
+
+  function MobiImg()
+  {
+  
+    var windowWidth = window.innerWidth;
+
+    if(windowWidth < 481)
+    {
+      document.getElementById('mobimg2').src ='https://images.samsung.com/is/image/samsung/assets/in/home/0208/Combo_720x1080-1.jpg?imwidth=720';
+    }
+   
+  }
+  
+  
+  
+  var windowWidth = window.innerWidth;
+
+    if(windowWidth < 481)
+    {
+      MobiImg()
+    }
