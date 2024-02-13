c75be8ab31829465443d8a8e3e73308ca4b24562
sarusarang <sarangsaru445@gmail.com>
2024-02-13 15:00:18 +0530

    hello

--- main.js
+++ main.js
@@ -1,75 +1,46 @@
-
-// MOBILE VIEW 
-
-
-// img-1
-
-function MobVideo()
-{
+// Function to set video source for mobile view
+function MobVideo() {
 
   var windowWidth = window.innerWidth;
-  
-  if(windowWidth < 481)
-  {
+  if (windowWidth < 481) {
     document.getElementById('mob').src ='Img/MOBILE VIEW.webm';
   }
- 
+
 }
-
-
-
-var windowWidth = window.innerWidth;
 
 window.onload = MobVideo()
 
- 
-
-
-
-
-// // img- 2
-
-  function MobImg()
-{
+// Function to set image source 1 for mobile view
+function MobImg() {
 
   var windowWidth = window.innerWidth;
-
-  if(windowWidth < 481)
-  {
+  if (windowWidth < 481) {
     document.getElementById('mobimg1').src ='https://images.samsung.com/is/image/samsung/assets/in/eureka2024/HOME_Live-Translate_Main-KV_720x1080_mo.jpg?imwidth=720';
   }
- 
+
 }
 
+// Function to set image source 2 for mobile view
+function MobiImg() {
 
-
-var windowWidth = window.innerWidth;
-  if(windowWidth < 481)
-  {
-    MobImg()
-  }
-
-
-// img - 3
-
-
-  function MobiImg()
-  {
-  
-    var windowWidth = window.innerWidth;
-
-    if(windowWidth < 481)
-    {
-      document.getElementById('mobimg2').src ='https://images.samsung.com/is/image/samsung/assets/in/home/0208/Combo_720x1080-1.jpg?imwidth=720';
-    }
-   
-  }
-  
-  
-  
   var windowWidth = window.innerWidth;
+  if (windowWidth < 481) {
+    document.getElementById('mobimg2').src ='https://images.samsung.com/is/image/samsung/assets/in/home/0208/Combo_720x1080-1.jpg?imwidth=720';
+  }
 
-    if(windowWidth < 481)
-    {
-      MobiImg()
-    }
+}
+
+window.onload = function() {
+
+  MobImg();
+  MobiImg();
+  
+  // Listen for the resize event and rerun the functions when the resolution changes
+  window.addEventListener('resize', function() {
+
+    MobVideo();
+    MobImg();
+    MobiImg();
+  });
+
+};
