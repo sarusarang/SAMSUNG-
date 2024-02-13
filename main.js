653cbe8485461b586d7cfc8237a3206ba454afe2
sarusarang <sarangsaru445@gmail.com>
2024-02-13 16:44:37 +0530

    ff

--- main.js
+++ main.js
@@ -1,46 +1,51 @@
 // Function to set video source for mobile view
-function MobVideo() {
+
+
+function Mob() {
 
   var windowWidth = window.innerWidth;
+
+
   if (windowWidth < 481) {
-    document.getElementById('mob').src ='Img/MOBILE VIEW.webm';
+
+    document.getElementById('mob').src ='Img/MOBILE VIEW.webm'
+    document.getElementById('video').load()
+   
   }
 
 }
 
-window.onload = MobVideo()
 
-// Function to set image source 1 for mobile view
-function MobImg() {
+// Function to set image sources for mobile view
+
+function MobImages() {
 
   var windowWidth = window.innerWidth;
+
   if (windowWidth < 481) {
+    
     document.getElementById('mobimg1').src ='https://images.samsung.com/is/image/samsung/assets/in/eureka2024/HOME_Live-Translate_Main-KV_720x1080_mo.jpg?imwidth=720';
-  }
-
-}
-
-// Function to set image source 2 for mobile view
-function MobiImg() {
-
-  var windowWidth = window.innerWidth;
-  if (windowWidth < 481) {
     document.getElementById('mobimg2').src ='https://images.samsung.com/is/image/samsung/assets/in/home/0208/Combo_720x1080-1.jpg?imwidth=720';
-  }
 
+  }
 }
+
+// Function to change on resize
 
 window.onload = function() {
 
-  MobImg();
-  MobiImg();
+  Mob()
+  MobImages();
+
   
-  // Listen for the resize event and rerun the functions when the resolution changes
-  window.addEventListener('resize', function() {
-    
-    MobVideo();
-    MobImg();
-    MobiImg();
-  });
+  function handleResize() {
+    Mob()
+    MobImages();
+  }
+
+
+  window.addEventListener('resize', handleResize);
+
 
 };
+
